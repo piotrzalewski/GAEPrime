4b1ba8473f1e2e0a6388e5a52299c32e2eae56ec
Piotr Zalewski <piotr.zalewski@unit9.com>
2015-01-02 11:30:36 +0100

    Trying add caching results

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -1,7 +1,7 @@
 
 get "/", forward: "/WEB-INF/pages/index.gtpl"
 get "/datetime", forward: "/datetime.groovy"
-get "/prime/@number?", forward: "/prime.groovy"
+get "/prime/@number?", forward: "/prime.groovy", cache: 10.minutes
 get "/table/@number?", forward: "/table.groovy"
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
 all "/_ah/start", forward: "/start.groovy"
